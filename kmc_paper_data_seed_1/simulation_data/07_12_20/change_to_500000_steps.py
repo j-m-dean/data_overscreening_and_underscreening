e071fe8d72cdcd595d5d4f8308468475e89c874e
Jacob Dean <jmd80@bath.ac.uk>
2021-03-25 19:04:32 +0000

    remove files

--- kmc_paper_data_seed_1/simulation_data/07_12_20/change_to_500000_steps.py
+++
@@ -1,6 +0,0 @@
-import json
-with open('input.json') as json_file:
-    data = json.load(json_file)
-data["number_of_kmc_steps"] = 500000
-with open('input.json', 'w') as outfile:
-    json.dump(data, outfile)
